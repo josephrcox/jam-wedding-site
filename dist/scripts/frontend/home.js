277d79f0657315b5b695f687874ecb248474b2c2
Joseph Cox <joeyhobart@outlook.com>
2023-09-13 21:14:47 -0400

    Google analytics

--- dist/scripts/frontend/home.js
+++ dist/scripts/frontend/home.js
@@ -69,6 +69,7 @@ const days = Math.floor(diff / 1000 / 60 / 60 / 24);
 const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
 const minutes = Math.floor(diff / 1000 / 60) % 60;
 
-countdown.innerHTML = ` 
+countdown.innerHTML = 'Saturday September 7, 2024<br />';
+countdown.innerHTML += ` 
 		<div>${days} <span>days - </span>${hours} <span>hours - </span> ${minutes} <span>minutes</span> left to go!</div>
 	`;
